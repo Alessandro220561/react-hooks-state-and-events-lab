1e4dce9cc0c6b5ceb2ff67a360a831c15c8beae4
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-05-26 14:07:14 -0400

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [ addToCart, setAddToCart ] = useState(false)
+
+  const liClass = addToCart ? "in-cart" : ""
+
+  const buttonText = addToCart ? "Remove From Cart" : "Add To Cart"
+  
+  const toggleAddToCart = () => {
+    setAddToCart( !addToCart )
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={ toggleAddToCart }>{ buttonText }</button>
     </li>
   );
 }
